a1cdbd054f5cda11bd5cdb45c8b8a40f740a3cdf
michael <mpthornton86@gmail.com>
2021-02-20 17:38:58 -0700

    updated constants

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,6 @@
 const burgerList = document.getElementById('burger-list')
 const burgerDetail = document.getElementById('burger-deets')
-const mealDetail = document.getElementById('meal-list')
-const main = document.getElementById('menu')
-const mealForm = document.getElementById('meal-form')
-const orderList = document.getElementById('orders')
+const burgerForm = document.getElementById('burger-form')
 
 
 BurgerApi.fetchBurgers()
